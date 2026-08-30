3a4344a0fd14a5c4d0044cfaa4e39107814f275e
Maicon André do Amaral Santos <maiconandre227789@gmail.com>
2026-08-30 20:59:15 -0300

    Atualiza dados fictícios para demonstração

--- backend/Banco de Dados/03_dados_teste.sql.sql
+++ backend/Banco de Dados/03_dados_teste.sql.sql
@@ -2,8 +2,8 @@ INSERT INTO alunos
 (nome, email, telefone, plano, status)
 VALUES
 (
-'Maicon Santos',
-'maicon@academia.com',
+'João da Silva',
+'joao.silva@exemplo.com',
 '(51)99999-9999',
 'Plano Premium',
 'Ativo'
